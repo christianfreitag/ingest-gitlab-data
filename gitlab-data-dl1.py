c623a40fe84a384af9a2b5803b3ad62c5aa030cd
Christian Freitag <christian.freitag.residente@tre-rn.jus.br>
2024-05-28 18:27:59 -0300

    FEAT: modifica para fazer ingestão em um postgres

--- gitlab-data-dl1.py
+++ gitlab-data-dl1.py
@@ -1,5 +1,6 @@
 import requests
-import sqlite3
+import psycopg2
+from psycopg2 import sql
 from dotenv import load_dotenv
 import os
 
@@ -18,15 +19,14 @@ SQL_RESERVED_WORDS = [
     'notnull', 'null', 'of', 'offset', 'on', 'or', 'order', 'outer', 'plan', 'pragma', 'primary', 'query', 'raise',
     'recursive', 'references', 'regexp', 'reindex', 'release', 'rename', 'replace', 'restrict', 'right', 'rollback',
     'row', 'savepoint', 'select', 'set', 'table', 'temp', 'temporary', 'then', 'to', 'transaction', 'trigger',
-    'union', 'unique', 'update', 'using', 'vacuum', 'values', 'view', 'virtual', 'when', 'where', 'with', 'without',
-    'commit','default'
+    'union', 'unique', 'update', 'using', 'vacuum', 'values', 'view', 'virtual', 'when', 'where', 'with', 'without'
 ]
 RESOURCES = {
     "projects": {
-        "projects": {"primary": {"project_id": 'INTEGER'}},
+        "projects": {"primary": {"project_id": 'INTEGER', "group_id": 'INTEGER'}},
         "issues": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
         "pipelines": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
-        "releases": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
+        "releases": {"primary": {"project_id": 'INTEGER'}},
         "milestones": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
         "labels": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
         "members": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
@@ -36,8 +36,8 @@ RESOURCES = {
         "protected_tags": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
         "boards": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
         "merge_requests": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
-        "repository/commits": {"primary": {"id": 'INTEGER', "project_id": 'INTEGER'}},
-        "repository/branches": {"primary": {"name": 'TEXT', "project_id": 'INTEGER'}},
+        "repository/commits": {"primary": {"id": 'VARCHAR(300)', "project_id": 'INTEGER'}},
+        "repository/branches": {"primary": {"name": 'VARCHAR(300)', "project_id": 'INTEGER'}},
     },
     "groups": {
         "groups": {"primary": {"group_id": 'INTEGER'}},
@@ -46,6 +46,16 @@ RESOURCES = {
         "repository_files": {"primary": {"id": 'INTEGER', "group_id": 'INTEGER'}},
     },
 }
+
+def create_connection():
+    connection = psycopg2.connect(
+        host="127.0.0.1",
+        port="54321",
+        database="postgres",
+        user="postgres",
+        password="1qaz3edc5tgb7ujm9ol."
+    )
+    return connection
 
 def get_gitlab_data(endpoint, per_page=100, page=1):
     try:
@@ -59,8 +69,8 @@ def get_gitlab_data(endpoint, per_page=100, page=1):
             data = response.json()
             total_pages = int(response.headers.get('X-Total-Pages', 0))
             return data, total_pages
-    except:
-        print(f"Failed to get data from {endpoint}")
+    except Exception as e:
+        print(f"Failed to get data from {endpoint}: {e}")
         return [], 0
 
 def create_tables(connection, RESOURCES):
@@ -70,24 +80,33 @@ def create_tables(connection, RESOURCES):
             for resource, info in resource_info.items():
                 table_name = f"{resource_type}_{resource}".replace("/", "_")
                 columns = ', '.join([f'{k} {v}' for k, v in info['primary'].items()])
-                cursor.execute(f'''CREATE TABLE IF NOT EXISTS {table_name} (
-                                    {columns},
-                                    PRIMARY KEY ({', '.join(info['primary'].keys())})
-                                )''')
-
+                cursor.execute(sql.SQL('''CREATE TABLE IF NOT EXISTS {} (
+                    {},
+                    PRIMARY KEY ({})
+                    )''').format(
+                        sql.Identifier(table_name),
+                        sql.SQL(columns),
+                        sql.SQL(', ').join(map(sql.Identifier, info['primary'].keys()))
+                    ))
     connection.commit()
+    cursor.close()
 
 def add_missing_columns(cursor, table_name, column_names):
     table_name = table_name.replace("/", "_")
-    cursor.execute(f"PRAGMA table_info({table_name})")
-    existing_columns = [column[1] for column in cursor.fetchall()]
+    cursor.execute(sql.SQL("SELECT column_name FROM information_schema.columns WHERE table_name = {}").format(sql.Literal(table_name)))
+    existing_columns = [row[0] for row in cursor.fetchall()]
 
     for column_name in column_names:
         if column_name not in existing_columns:
-            cursor.execute(f'ALTER TABLE "{table_name}" ADD COLUMN "{column_name}" TEXT')
+            cursor.execute(sql.SQL('ALTER TABLE {} ADD COLUMN {} TEXT').format(
+                sql.Identifier(table_name),
+                sql.Identifier(column_name)
+            ))
 
 def build_where_clause_and_params(primary_keys, entry):
-    where_clause = ' AND '.join([f"{key} = ?" for key in primary_keys.keys()])
+    where_clause = sql.SQL(' AND ').join(
+        sql.SQL("{} = %s").format(sql.Identifier(key)) for key in primary_keys.keys()
+    )
     params = tuple(entry.get(key) for key in primary_keys.keys())
     return where_clause, params
 
@@ -95,9 +114,13 @@ def clean_values(entry):
     return [str(value) if isinstance(value, (dict, list)) else value for value in entry.values()]
 
 def execute_sql(cursor, table_name, column_names, values):
-    placeholders = ', '.join(['?'] * len(values))
-    escaped_column_names = [f'"{column}"' if column in SQL_RESERVED_WORDS else column for column in column_names]
-    cursor.execute(f"INSERT INTO {table_name}({', '.join(escaped_column_names)}) VALUES ({placeholders})", values)
+    placeholders = sql.SQL(', ').join(sql.Placeholder() * len(values))
+    escaped_column_names = [sql.Identifier(column) if column in SQL_RESERVED_WORDS else sql.Identifier(column) for column in column_names]
+    cursor.execute(sql.SQL("INSERT INTO {} ({}) VALUES ({})").format(
+        sql.Identifier(table_name),
+        sql.SQL(', ').join(escaped_column_names),
+        placeholders
+    ), values)
 
 def insert_data(connection, table_name, data, RESOURCES, parent_id):
     if len(data) > 0 and isinstance(data[0], dict):
@@ -107,6 +130,7 @@ def insert_data(connection, table_name, data, RESOURCES, parent_id):
         cursor = connection.cursor()
 
         for entry in data:
+
             column_names = list(entry.keys())
             if (data_parent + '_id') not in column_names:
                 column_names.append((data_parent + '_id'))
@@ -117,7 +141,10 @@ def insert_data(connection, table_name, data, RESOURCES, parent_id):
 
             where_clause, params = build_where_clause_and_params(primary_from_RESOURCES, entry)
             table_name = table_name.replace("/", "_")
-            cursor.execute(f"SELECT {(data_parent + '_id')} FROM {table_name} WHERE {where_clause}", params)
+            cursor.execute(sql.SQL("SELECT * FROM {} WHERE {}").format(
+                sql.Identifier(table_name),
+                where_clause
+            ), params)
 
             if not cursor.fetchone():
                 cleaned_values = clean_values(entry)
@@ -129,7 +156,7 @@ def insert_data(connection, table_name, data, RESOURCES, parent_id):
 
 def main():
     # Create a connection to the database
-    connection = sqlite3.connect('gitlab_data.db')
+    connection = create_connection()
     create_tables(connection, RESOURCES)
 
     # Get projects data
@@ -141,7 +168,7 @@ def main():
         proj['project_id'] = proj['id']
         proj['group_id'] = proj['namespace']['id']
         proj.pop('namespace', None)
-    insert_data(connection, "projects", projects_data, RESOURCES, None)
+    insert_data(connection, "projects_projects", projects_data, RESOURCES, None)
 
     # Get groups data
     groups_data, total_pages = get_gitlab_data("groups", 100, 1)
@@ -151,11 +178,12 @@ def main():
     for gp in groups_data:
         gp['group_id'] = gp['id']
         gp.pop('projects', None)
-    insert_data(connection, "groups", groups_data, RESOURCES, None)
+    insert_data(connection, "groups_groups", groups_data, RESOURCES, None)
 
     # Insert data into tables
     for resource_type, RESOURCES_list in RESOURCES.items():
         for index, resource in enumerate(RESOURCES_list):
+            if(index != 0):
                 if resource_type == "projects":
                     if projects_data:
                         for project in projects_data:
